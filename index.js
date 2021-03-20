e4526d88e57ba96ef70dade59bde2780eae51842
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-20 20:07:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-isBalanced("()[]")
-//true
-isBalanced("([])")
-//true
-isBalanced("({)}")
-//false
-isBalanced("{[}")
-//false
+//isBalanced("()[]")
+//true--> done
 
-function isMatch( left, right ) {
- return ( left === '(' && right === ')' || left === '[' && right === ']' || left === '{' && right === '}' )
-}
+//isBalanced("([])")
+//true-->
+
+//isBalanced("({)}")
+//false-->
+
+//isBalanced("{[}")
+//false-->
+
+
+describe('#isBalanced', function() {
+	it('returns true if string is balanced', function(){
+		let str = '([])[{}]{(())}'
+		expect(isBalanced(str)).toEqual(true)
+	})
+});
+
+describe('#isBalanced', function() {
+	it('returns false if string is not balanced', function(){
+		let str = '([])[{}]{(([))}'
+		expect(isBalanced(str)).toEqual(false)
+	})
+});
+
+describe('#isBalanced', function() {
+	it('returns false even if string has all brackets matched', function(){
+		let str = '([])[{}]{([)]}'
+		expect(isBalanced(str)).toEqual(false)
+	})
+});
